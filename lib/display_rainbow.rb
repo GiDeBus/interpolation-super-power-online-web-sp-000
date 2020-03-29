4bbda6fdd1bf07aac9f8aa937c69aca841733083
immutable-integer-8885 <gidelbusto@gmail.com>
2020-03-29 01:51:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,8 @@
 # Write your #display_rainbow method here
 
+
+colors = ['red','orange','yellow','green','blue','indigo','violet']
+
 display_rainbow()
-  colors= ['red','orange','yellow','green','blue','indigo','violet']
-  
 
 end
